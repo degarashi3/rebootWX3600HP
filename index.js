3fcec736d747acaeb503a874b5d6fcccce7b3e7b
degarashi3 <debug.log@gmail.com>
2022-05-24 21:43:36 +0900

    debug

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const puppeteer = require('puppeteer');
 
 (async () => {
@@ -6,7 +7,7 @@ const puppeteer = require('puppeteer');
   })
   const page = await browser.newPage();
   await page.setDefaultNavigationTimeout(0);
-  await page.goto('http://<your-WX3600HP-IP-address>/page/login_main.html');
+  await page.goto('http://<ipaddress>/page/login_main.html');
 
   await page.keyboard.press('Tab');
   await page.keyboard.type('admin');
@@ -18,58 +19,26 @@ const puppeteer = require('puppeteer');
     await page.keyboard.press('Enter')
   ]);
 
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
+  for (let i = 0; i < 28; i++) {
+    await page.keyboard.press('Tab');
+  }
   await Promise.all([
     page.waitForNavigation({ waitUntil: 'networkidle0' }),
     await page.keyboard.press('Enter')
   ]);
 
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
+  for (let i = 0; i < 6; i++) {
+    await page.keyboard.press('Tab');
+  }
   await page.keyboard.press('Enter');
 
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
+  for (let i = 0; i < 7; i++) {
+    await page.keyboard.press('Tab');
+  }
   await Promise.all([
     page.waitForNavigation({ waitUntil: 'networkidle0' }),
     await page.keyboard.press('Enter')
   ]);
-
+  await page.screenshot({ path: 'example.png' });
   await browser.close();
 })();
